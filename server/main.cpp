cef6d56dcc2f79fb773cb693ca2314e5398e8894
James Stevenson <james@stev.org>
2014-07-11 11:35:09 +0100

    Added Program arguments and variable pid file and socket locations

--- server/main.cpp
+++ server/main.cpp
@@ -1,20 +1,76 @@
 
 #include <main.h>
 
+void print_help(FILE *fp, const char *app)
+{
+	fprintf(fp, "Usage: %s <options>\n", app);
+	fprintf(fp, "\n");
+	fprintf(fp, " -h --help              Print this help and exit\n");
+	fprintf(fp, " -p --pid <filename>    Use the filename as pid file\n");
+	fprintf(fp, " -s --socket <filename> Use the filename as a unix socket file for services\n");
+	fprintf(fp, "\n");
+	fprintf(fp, "\n");
+}
+
 int main(int argc, char **argv)
 {
 	LogManager::Init();
 	RTSPServer *RServer = NULL;
 	RTSPHandler Handler;
 	ServerManager Manager(&Handler);
-	ServerUnix Unix("/tmp/CameraServer");
+	std::string LocSocket = "/tmp/CameraServer";
+	std::string LocPidFile = "";
+	PIDFile *PidFile = NULL;
+	bool AlwaysLog = false;
+
+	const char *opts = "h";
+	int longindex = 0;
+	int c = 0;
+	struct option loptions[]
+	{
+		{"help", 0, 0, 'h'},
+		{"pid", 1, 0, 'p'},
+		{"socket", 1, 0, 's'},
+		{0, 0, 0, 0}
+	};
+
+	while( (c = getopt_long(argc, argv, opts, loptions, &longindex)) >= 0)
+	{
+		switch(c)
+		{
+			case 'h':
+				print_help(stdout, argv[0]);
+				exit(EXIT_SUCCESS);
+				break;
+			case 'p':
+				LocPidFile = optarg;
+				break;
+			case 's':
+				LocSocket = optarg;
+				break;
+			default:
+				break;
+		}
+	}
 
 	//Add Logging
-	if (isatty(fileno(stdout)) == 1)
+	if (isatty(fileno(stdout)) == 1 || AlwaysLog == true)
 	{
 		LogManager::Add(new LogStdout());
 	}
 
+
+	//Setup PID File
+	if (LocPidFile != "")
+	{
+		PidFile = new PIDFile(LocPidFile);
+		if (PidFile->Create() == false)
+		{
+			LogError("Cannot Create Pid File - %s", LocPidFile.c_str());
+			exit(EXIT_FAILURE);
+		}
+		LogInfo("Created PIDFile: %s", LocPidFile.c_str());
+	}
 
 	//Start RTSP Server
 	gst_init (&argc, &argv);
@@ -25,7 +81,8 @@ int main(int argc, char **argv)
 	RServer->PipelineAdd("/test", "( videotestsrc ! x264enc ! rtph264pay name=pay0 pt=96 )");
 
 	//Start Our Local Services
-	LogDebug("main - Starting Services");
+	LogDebug("Serverice Listen On: %s", LocSocket.c_str());
+	ServerUnix Unix(LocSocket);
 	Manager.ServerAdd(&Unix);
 	Handler.Wait();
 	Manager.ServerRemove(&Unix);
@@ -34,6 +91,7 @@ int main(int argc, char **argv)
 
 	//Cleanup!
 	delete RServer;
+	delete PidFile;
 	gst_deinit();
 	LogManager::RemoveAll(true);
 	return 0;
--- server/main.h
+++ server/main.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <getopt.h>
 
 #include <string>
 
