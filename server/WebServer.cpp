de837d2ae54c5aa333f3a54dcbb581020eb89b76
James Stevenson <james@stev.org>
2015-07-03 14:29:31 +0100

    Whitespace cleanup

--- server/WebServer.cpp
+++ server/WebServer.cpp
@@ -240,9 +240,7 @@ bool WebServer::Exec()
 
 		if (sigprocmask(SIG_UNBLOCK, &all, NULL) < 0)
 			abort();
-			
 
-		
 		if (execl("/usr/bin/xsp4", "/usr/bin/xsp4", "--nonstop", "--root", "/home/james/src/camera/WebUI", "--port", sport.c_str(), NULL) < 0)
 		{
 			printf("execl failed: %s\n", strerror(errno));
