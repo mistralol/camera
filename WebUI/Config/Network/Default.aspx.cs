28e0a592c525ea0877d0ddfa45703e2e4f83055f
James Stevenson <james@stev.org>
2015-09-05 14:16:37 +0100

    Added Network default Page

---
+++ WebUI/Config/Network/Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using WebUI.Code;
+
+namespace WebUI.Config.Network
+{
+    public partial class Default : AppPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
