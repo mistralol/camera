74023083aab96c4d547f9314aeb8499cf4177fcf
James Stevenson <james@stev.org>
2015-09-05 14:16:09 +0100

    Added WebServer Options to web gui

---
+++ WebUI/Config/Network/WebServer.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebServer.aspx.cs" Inherits="WebUI.Config.Network.WebServer" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <table>
+        <tr>
+            <td>Web Server Port</td>
+            <td><asp:TextBox runat="server" ID="txtPort"></asp:TextBox></td>
+            <td>
+                <asp:RequiredFieldValidator runat="server" ID="valPort" ControlToValidate="txtPort" ErrorMessage="Port is required"></asp:RequiredFieldValidator>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <asp:Button runat="server" ID="btnSave" Text="Save" onclick="btnSave_Click" />
+                <asp:Button runat="server" ID="btnCancel" Text="Cancel" 
+                    CausesValidation="false" onclick="btnCancel_Click" />
+            </td>
+            <td>
+                <asp:Label runat="server" ID="lblError"></asp:Label>
+            </td>
+        </tr>
+    </table>
+
+</asp:Content>
---
+++ WebUI/Config/Network/WebServer.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using WebUI.Code;
+
+namespace WebUI.Config.Network
+{
+    public partial class WebServer : AppPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblError.Text = "";
+            if (IsPostBack == false)
+            {
+                Bind();
+            }
+        }
+
+        protected void Bind()
+        {
+            int port = 0;
+            
+            int ret = Camera.WebServerGetPort(out port);
+            if (ret < 0)
+                throw(new CameraClientException(ret));
+
+            txtPort.Text = port.ToString();
+        }
+
+        protected void UnBind()
+        {
+            bool success = true;
+            int port = Convert.ToInt16(txtPort.Text);
+
+            if (Camera.WebServerSetPort(port) < 0)
+                success = false;
+
+            if (success)
+            {
+                lblError.Text = "Saved";
+            }
+            else
+            {
+                lblError.Text = "Failed";
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (IsValid == false)
+            {
+                lblError.Text = "* Invalid values";
+                return;
+            }
+
+            UnBind();
+            Bind();
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            Bind();
+        }
+    }
+}
