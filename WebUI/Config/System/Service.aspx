b8c598a3bea5e9f6d7260ba37afdaa5cbf3a4b22
James Stevenson <james@stev.org>
2015-11-13 21:24:23 +0000

    Added Service page to Shutdown, Reboot and stop service

---
+++ WebUI/Config/System/Service.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Service.aspx.cs" Inherits="WebUI.Config.System.Service" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <asp:Panel runat="server" ID="pnlStart">
+        <asp:Button runat="server" ID="btnStopServer" Text="Stop Server" 
+            onclick="btnStopServer_Click" /><br />
+        <br />
+        <asp:Button runat="server" ID="btnShutdown" Text="Shutdown" onclick="btnShutdown_Click" /><br />
+        <br />
+        <asp:Button runat="server" ID="btnReboot" Text="Reboot" onclick="btnReboot_Click" /><br />
+        <br />
+    </asp:Panel>
+
+    <asp:Panel runat="server" ID="pnlDone" Visible="false">
+        Please Wait
+    </asp:Panel>
+
+    <asp:Panel runat="server" ID="pnlError" Visible="false">
+        Error: Unable to complete operation
+        <asp:Label runat="server" ID="lblError"></asp:Label>
+    </asp:Panel>
+
+</asp:Content>
---
+++ WebUI/Config/System/Service.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using WebUI.Code;
+
+namespace WebUI.Config.System
+{
+    public partial class Service : AppPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            pnlError.Visible = false;
+        }
+
+        protected void btnStopServer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Camera.Quit();
+                pnlDone.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                pnlStart.Visible = false;
+                pnlError.Visible = true;
+                lblError.Text = "Error: " + ex.Message;
+            }
+
+        }
+
+        protected void btnShutdown_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Camera.SystemShutdown();
+                pnlDone.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                pnlStart.Visible = false;
+                pnlError.Visible = true;
+                lblError.Text = "Error: " + ex.Message;
+            }
+        }
+
+        protected void btnReboot_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Camera.SystemReboot();
+                pnlDone.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                pnlStart.Visible = false;
+                pnlError.Visible = true;
+                lblError.Text = "Error: " + ex.Message;
+            }
+        }
+    }
+}
