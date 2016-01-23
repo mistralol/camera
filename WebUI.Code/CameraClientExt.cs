9e443846216a70dd7629154823664f71fbc479f0
James Stevenson <james@stev.org>
2016-01-23 14:39:11 +0000

    Added CameraClient extenstions like string.format to CameraClient class

---
+++ WebUI.Code/CameraClientExt.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebUI.Code
+{
+    public static class CameraClientExt
+    {
+        public static void LogDebug(this CameraClient Camera, string fmt, params object[] args)
+        {
+            Camera.LogDebug(string.Format(fmt, args));
+        }
+
+        public static void LogInfo(this CameraClient Camera, string fmt, params object[] args)
+        {
+            Camera.LogInfo(string.Format(fmt, args));
+        }
+
+        public static void LogNotice(this CameraClient Camera, string fmt, params object[] args)
+        {
+            Camera.LogNotice(string.Format(fmt, args));
+        }
+
+        public static void LogWarning(this CameraClient Camera, string fmt, params object[] args)
+        {
+            Camera.LogWarning(string.Format(fmt, args));
+        }
+
+        public static void LogError(this CameraClient Camera, string fmt, params object[] args)
+        {
+            Camera.LogError(string.Format(fmt, args));
+        }
+
+        public static void LogCrtical(this CameraClient Camera, string fmt, params object[] args)
+        {
+            Camera.LogCrtical(string.Format(fmt, args));
+        }
+
+        public static void LogAlert(this CameraClient Camera, string fmt, params object[] args)
+        {
+            Camera.LogAlert(string.Format(fmt, args));
+        }
+
+        public static void LogEmergancy(this CameraClient Camera, string fmt, params object[] args)
+        {
+            Camera.LogEmergancy(string.Format(fmt, args));
+        }
+    }
+}
+
+
